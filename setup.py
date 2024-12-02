440680f599b32e0f26f31584d709baa005e75a73
Evgeny Ockatiev <evgeny.ockatiev@gmail.com>
2024-12-02 10:48:38 +0300

    setup.py correction.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="SnmpPyLite",
+    name="snmp_py_lite",
     version="0.1.0",
     author="Evgeny Ockatiev",
     author_email="evgeny.ockatiev@gmail.com",
